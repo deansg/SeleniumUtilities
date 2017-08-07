afcb2dc20af6a2ba08862c89e8e6f380fe089733
Dean Gurvitz <deansg@gmail.com>
2017-08-07 21:23:55 +0300

    Add the Extensions class with all SelectArray related functions

---
+++ SeleniumUtilities/Extensions.cs
@@ -0,0 +1,219 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Threading.Tasks;
+
+namespace SeleniumUtilities
+{
+    public static class Extensions
+    {
+        #region Public Methods
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector. This is more efficient than a 
+        /// ToArray/ToList function since it counts on the listCollection's count. In addition, it uses the 
+        /// listCollection's indexer to guarantee that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArray<TOriginal, TOutput>(this IReadOnlyList<TOriginal> listCollection,
+            Func<TOriginal, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            for (int i = 0; i < listCollection.Count; i++)
+            {
+                returnedArr[i] = selector(listCollection[i]);
+            }
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector. This is more efficient than a 
+        /// ToArray/ToList function since it counts on the listCollection's count. In addition, it uses the 
+        /// listCollection's indexer to guarantee that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArray<TOriginal, TOutput>(this IReadOnlyList<TOriginal> listCollection,
+            Func<TOriginal, int, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            for (int i = 0; i < listCollection.Count; i++)
+            {
+                returnedArr[i] = selector(listCollection[i], i);
+            }
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector. This is more efficient than a 
+        /// ToArray/ToList function since it counts on the listCollection's count. In addition, it uses the 
+        /// listCollection's indexer to guarantee that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayList<TOriginal, TOutput>(this IList<TOriginal> listCollection,
+            Func<TOriginal, TOutput> selector)
+        {
+            // Objects implementing IList should also be implementing IReadOnlyList
+            return ((IReadOnlyList<TOriginal>)listCollection).SelectArray(selector);
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector. This is more efficient than a 
+        /// ToArray/ToList function since it counts on the listCollection's count. In addition, it uses the 
+        /// listCollection's indexer to guarantee that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayList<TOriginal, TOutput>(this IList<TOriginal> listCollection,
+            Func<TOriginal, int, TOutput> selector)
+        {
+            // Objects implementing IList should also be implementing IReadOnlyList
+            return ((IReadOnlyList<TOriginal>)listCollection).SelectArray(selector);
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector in parallel. This is more efficient 
+        /// than a ToArray/ToList function since it counts on the listCollection's count, and also utilizes the array's
+        /// memoery to apply the selector in parallel. In addition, it uses the listCollection's indexer to guarantee 
+        /// that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayParallel<TOriginal, TOutput>(this IReadOnlyList<TOriginal> listCollection,
+            Func<TOriginal, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            Parallel.For(0, listCollection.Count, i => returnedArr[i] = selector(listCollection[i]));
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector in parallel. This is more efficient 
+        /// than a ToArray/ToList function since it counts on the listCollection's count, and also utilizes the array's
+        /// memoery to apply the selector in parallel. In addition, it uses the listCollection's indexer to guarantee 
+        /// that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayParallel<TOriginal, TOutput>(this IReadOnlyList<TOriginal> listCollection,
+            Func<TOriginal, int, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            Parallel.For(0, listCollection.Count, i => returnedArr[i] = selector(listCollection[i], i));
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector in parallel. This is more efficient 
+        /// than a ToArray/ToList function since it counts on the listCollection's count, and also utilizes the array's
+        /// memoery to apply the selector in parallel. In addition, it uses the listCollection's indexer to guarantee 
+        /// that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayParallelList<TOriginal, TOutput>(this IList<TOriginal> 
+            listCollection, Func<TOriginal, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            Parallel.For(0, listCollection.Count, i => returnedArr[i] = selector(listCollection[i]));
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Creates an array of objects of type TOutput using the given selector in parallel. This is more efficient 
+        /// than a ToArray/ToList function since it counts on the listCollection's count, and also utilizes the array's
+        /// memoery to apply the selector in parallel. In addition, it uses the listCollection's indexer to guarantee 
+        /// that the new array element order is as intended.
+        /// </summary>
+        /// <typeparam name="TOriginal"></typeparam>
+        /// <typeparam name="TOutput"></typeparam>
+        /// <param name="listCollection"></param>
+        /// <param name="selector"></param>
+        /// <returns></returns>
+        [Pure]
+        public static TOutput[] SelectArrayParallelList<TOriginal, TOutput>(this IList<TOriginal>
+            listCollection, Func<TOriginal, int, TOutput> selector)
+        {
+            var returnedArr = new TOutput[listCollection.Count];
+            Parallel.For(0, listCollection.Count, i => returnedArr[i] = selector(listCollection[i], i));
+            return returnedArr;
+        }
+
+        /// <summary>
+        /// Returns a collection of texts of IWebElements using the SelectArray functions
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        [Pure]
+        public static IReadOnlyList<string> Texts(this IReadOnlyList<IWebElement> elements)
+        {
+            return elements.SelectArray(element => element.Text);
+        }
+
+        /// <summary>
+        /// Returns a collection of texts of IWebElements using the SelectArray functions
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        [Pure]
+        public static IReadOnlyList<string> Texts(this IList<IWebElement> elements)
+        {
+            return elements.SelectArrayList(element => element.Text);
+        }
+
+        /// <summary>
+        /// Returns a collection of texts of IWebElements using the SelectArrayParallel function
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        [Pure]
+        public static IReadOnlyList<string> TextsParallel(this IReadOnlyList<IWebElement> elements)
+        {
+            return elements.SelectArrayParallel(element => element.Text);
+        }
+
+        /// <summary>
+        /// Returns a collection of texts of IWebElements using the SelectArrayParallelList function
+        /// </summary>
+        /// <param name="elements"></param>
+        /// <returns></returns>
+        [Pure]
+        public static IReadOnlyList<string> TextsParallel(this IList<IWebElement> elements)
+        {
+            return elements.SelectArrayParallelList(element => element.Text);
+        }
+
+        #endregion
+    }
+}
