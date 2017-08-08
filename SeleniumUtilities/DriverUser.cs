cab16421f5b4141980eb94594b4b88af39cb34cf
Dean Gurvitz <deansg@gmail.com>
2017-08-08 21:56:20 +0300

    Add a WebsiteWrapperBase class

--- SeleniumUtilities/DriverUser.cs
+++ SeleniumUtilities/DriverUser.cs
@@ -4,10 +4,13 @@ namespace SeleniumUtilities
 {
     /// <summary>
     /// An abstract base class for classes that use an IWebDriver. These can usually include logical automation
-    /// representations of different pages or windows in a website, but usually not the website-wrapper itself.
+    /// representations of different pages or windows in a website. The website-wrapper itself should inherit from
+    /// the deriving subclass, WebsiteWrapperBase
     /// </summary>
     public abstract class DriverUser
     {
+        #region Constructors
+
         protected DriverUser(IWebDriver driver)
         {
             Driver = driver;
@@ -20,8 +23,20 @@ namespace SeleniumUtilities
             WaitUntil = driverUser.WaitUntil;
         }
 
-        public IWebDriver Driver { get; }
+        #endregion
 
-        public WaitUntil WaitUntil { get; }
+        #region Properties
+
+        /// <summary>
+        /// The inner IWebDriver used for most Selenium operations
+        /// </summary>
+        protected IWebDriver Driver { get; }
+
+        /// <summary>
+        /// A WaitUntil object linked to the Driver object
+        /// </summary>
+        protected WaitUntil WaitUntil { get; }
+
+        #endregion
     }
 }
---
+++ SeleniumUtilities/WebsiteWrapperBase.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace SeleniumUtilities
+{
+    /// <summary>
+    /// An abstract base class for classes that represent a logical website automated by Selenium. It inherits the
+    /// DriverUser class, but is also responsible for Disposing the IWebDriver object, and increases its visibilty
+    /// </summary>
+    public abstract class WebsiteWrapperBase : DriverUser, IDisposable
+    {
+        protected WebsiteWrapperBase(IWebDriver driver) : base(driver)
+        {
+        }
+
+        /// <summary>
+        /// The IWebDriver used for most Selenium operations
+        /// </summary>
+        public new IWebDriver Driver { get { return base.Driver; } }
+
+        /// <summary>
+        /// Disposes the current IWebDriver if it isn't null
+        /// </summary>
+        public void Dispose()
+        {
+            if (Driver != null)
+            {
+                Driver.Dispose();
+            }
+        }
+    }
+}
